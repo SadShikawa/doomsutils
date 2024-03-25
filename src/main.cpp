b29c99156ad6f85843afeac170065114421be909
ignDoom <pata150@email.cz>
2024-03-25 18:30:37 +0100

    mod compatibility
    adds further compatibility to some mods

--- src/main.cpp
+++ src/main.cpp
@@ -29,21 +29,23 @@ class $modify(MenuLayer) {
 		auto socialmediamenu = this->getChildByID("social-media-menu"); // get social media menu thing
 
 		// edits social menu by deleting some stuff and scaling it down a bit
-		socialmediamenu->removeChildByID("robtop-logo-button");
-		socialmediamenu->removeChildByID("youtube-button");
-		socialmediamenu->removeChildByID("twitch-button");
-		socialmediamenu->setScale(0.9);
-		// get discord
-		auto discordbutton = socialmediamenu->getChildByID("discord-button");
-		// get fb
-		auto fbbutton = socialmediamenu->getChildByID("facebook-button");
-		// get twitter
-		auto twitterbutton = socialmediamenu->getChildByID("twitter-button");
-		// move discord button and social media menu parent
-		discordbutton->setPosition(30, 30);
-		fbbutton->setPosition(-27, 30);
-		twitterbutton->setPosition(1.5, 30);
-		socialmediamenu->setPosition(59, -6);
+		if(Loader::get()->isModLoaded("skyward.no_robtop_logo") == false){
+			socialmediamenu->removeChildByID("robtop-logo-button");
+			socialmediamenu->removeChildByID("youtube-button");
+			socialmediamenu->removeChildByID("twitch-button");
+			socialmediamenu->setScale(0.9);
+			// get discord
+			auto discordbutton = socialmediamenu->getChildByID("discord-button");
+			// get fb
+			auto fbbutton = socialmediamenu->getChildByID("facebook-button");
+			// get twitter
+			auto twitterbutton = socialmediamenu->getChildByID("twitter-button");
+			// move discord button and social media menu parent
+			discordbutton->setPosition(30, 30);
+			fbbutton->setPosition(-27, 30);
+			twitterbutton->setPosition(1.5, 30);
+			socialmediamenu->setPosition(59, -6);
+		}
 		
 		// moves the player button thing with username
 		auto profilemenu = this->getChildByID("profile-menu");
@@ -60,22 +62,25 @@ class $modify(MenuLayer) {
 		// CHANG twitter
 		
 		if(Mod::get()->getSettingValue<bool>("old-twitter-icon") == false) {
-			// create vars for buttns idk
+				// create vars for buttns idk
 			auto xButton = socialmediamenu->getChildByID("twitter-button");
-
-
 			LinkHandler* lh = new LinkHandler();
 			auto spr = CCSprite::create("DU_twitterBtn.png"_spr);
 			auto btn = CCMenuItemSpriteExtra::create(spr, socialmediamenu, lh, menu_selector(LinkHandler::openLink));
+			if(Loader::get()->isModLoaded("skyward.no_robtop_logo") == false){
 
+				btn->setPosition(ccp(1.5f, 30.0f));
+				spr->setScale(0.825f);
 
-			btn->setPosition(ccp(1.5f, 30.0f));
-			spr->setScale(0.825f);
+			}else{
+
+				btn->setPosition(ccp(41.6f, 39.6f));
+				spr->setScale(0.825f);
+			}
 
 			xButton->removeFromParentAndCleanup(true);
 			btn->setID("new-twitter-button");
 			socialmediamenu->addChild(btn);
-
 		};
 
 		// shortcut button
